e3c7ab0e8273b3ab1baf91d4c7730a43af8352ac
Luisfont13 <luisfont349@gmail.com>
2025-02-11 22:56:46 +0000

    comid

--- script.js
+++ script.js
@@ -5,3 +5,9 @@ document.addEventListener('DOMContentLoaded', function() {
 function mostrarAlerta() {
     alert('Hola, ¡bienvenido a mi sitio web!');
 }
+
+function cambiarColor() {
+    const colores = ['#FF6347', '#FFD700', '#ADFF2F', '#20B2AA', '#1E90FF'];
+    const colorAleatorio = colores[Math.floor(Math.random() * colores.length)];
+    document.body.style.backgroundColor = colorAleatorio;
+}
