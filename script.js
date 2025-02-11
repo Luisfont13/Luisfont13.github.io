d2a9223d00c0efd6eaf3a1076b942924431af529
Luisfont13 <luisfont349@gmail.com>
2025-02-11 22:46:08 +0000

    Mejoras y menú lateral agregado

---
+++ script.js
@@ -0,0 +1,7 @@
+document.addEventListener('DOMContentLoaded', function() {
+    console.log('Página cargada exitosamente');
+});
+
+function mostrarAlerta() {
+    alert('Hola, ¡bienvenido a mi sitio web!');
+}
